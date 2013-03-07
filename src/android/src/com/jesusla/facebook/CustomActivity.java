0dcf4c552b8591afe5bd034c40137f3b37e776f8
Michael Scholz <mscholz@sgn.com>
2013-03-07 14:28:39 -0800

    Fixed ane arch to create activity on demand instead of persistent

--- src/android/src/com/jesusla/facebook/CustomActivity.java
+++ src/android/src/com/jesusla/facebook/CustomActivity.java
@@ -9,36 +9,79 @@ import com.facebook.widget.*;
 import junit.framework.Assert;
 import com.jesusla.ane.Extension;
 
+
 public class CustomActivity extends Activity {
-  static public CustomActivity staticReference;
+
+  private static int activityCount = 0;
+  private int activityId = 0;
+
+  // loginActivity is persistent, except the OS kills it off whenever it feels like
+  // - turns out that when instrumented, this is happening often
+  private static CustomActivity loginActivity;
+
+  // dialogActivity closes and finishs every time it runs - should not return on resume 
+  // - unless loginActivity was previous nuked by OS, then it should migrate
+  private static CustomActivity dialogActivity;
+
   private UiLifecycleHelper uiHelper;
+  private Session.StatusCallback statusCallback;
+  private WebDialog.OnCompleteListener dialogCallback;
   
   public CustomActivity() {
-  	// the fb dialog calls have to execute in this context; leaving the activity running and passing this reference back allows it
-	  staticReference = this;
-    new RemapResourceIds(this);
+    activityId = ++activityCount;
+    Extension.debug("CustomActivity(): %d", activityId);
   }
-  
+
+  public static CustomActivity getLoginActivity() {
+    return loginActivity;
+  }
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
-    Extension.debug("CustomActivity:onCreate");
+    Extension.debug("CustomActivity %d:onCreate", activityId);
+    new RemapResourceIds(this);
     super.onCreate(savedInstanceState);
 
-	  Session.StatusCallback statusCallback = new Session.StatusCallback() {
+    statusCallback = new Session.StatusCallback() {
       @Override
-	    public void call(Session session, SessionState state, Exception exception) {
-	      Extension.debug("CustomActivity:onSessionStateChange(%s) [e:%s]", state, exception);
-		    // forward this back to the caller for asynch dispatch
-		    FacebookLib.staticReference.sessionStatusCallback.call(session,state,exception);
-	  	  if (state == SessionState.CLOSED_LOGIN_FAILED) {
-          Extension.debug("CustomActivity:onCreateCustomActivity.staticReference.finish()");
-	        CustomActivity.staticReference.finish();
-	      }
-	      if ((state == SessionState.OPENED)||(state == SessionState.OPENED_TOKEN_UPDATED)) {
-	      }
-	    }
-	  };
-	
+      public void call(Session session, SessionState state, Exception exception) {
+        Extension.debug("CustomActivity:onSessionStateChange(%s) [e:%s]", state, exception);
+        // forward this back to the caller for asynch dispatch
+        FacebookLib.sessionStatusCallback.call(session,state,exception);
+        if (state == SessionState.CLOSED_LOGIN_FAILED) {
+          if (loginActivity != null) {
+            //Extension.debug("loginActivity.finish() called");
+            loginActivity.finish();  
+          }
+        }
+        if ((state == SessionState.OPENED)||(state == SessionState.OPENED_TOKEN_UPDATED)) {
+        }
+      }
+    };
+    
+    dialogCallback = new WebDialog.OnCompleteListener() {
+      @Override
+      public void onComplete(Bundle values, FacebookException error) {
+        // forward this back to the caller for asynch dispatch
+        FacebookLib.dialogCompleteCallback.onComplete(values,error);
+        
+        //  Extension.debug("dialogCompleteCallback loginActivity %d dialogActivity %d", 
+        //      loginActivity != null ? loginActivity.activityId : -1, 
+        //      dialogActivity != null ? dialogActivity.activityId : -1);
+        
+        // if these activities ended up the same, keep the activity alive as the loginActivity
+        if (loginActivity != dialogActivity) {
+          //Extension.debug("dialogActivity.finish() called");
+          dialogActivity.finish();  
+        }
+        else {
+          // Extension.debug("dialogActivity.finish() NOT called");
+        }
+        // but the dialogActivity is done
+        dialogActivity = null;
+      }
+    };
+
 	  // the bulk of the fb boilerplate is now provided
 	  uiHelper = new UiLifecycleHelper(this, statusCallback);
     uiHelper.onCreate(savedInstanceState);
@@ -46,59 +89,103 @@ public class CustomActivity extends Activity {
 	  Session session = Session.getActiveSession();
 	  if (FacebookLib.isSessionValid() == false) {
 	    // if this player has an old access token, try to migrate it forward
-	    AccessToken oldAccessToken = FacebookLib.staticReference.oldAccessToken;
-        if (oldAccessToken != null) {
-          session = Session.openActiveSessionWithAccessToken(this, oldAccessToken, null);
-          Assert.assertEquals(session, Session.getActiveSession());
-        }
-	    }
+	    AccessToken oldAccessToken = FacebookLib.oldAccessToken;
+      if (oldAccessToken != null) {
+        session = Session.openActiveSessionWithAccessToken(this, oldAccessToken, null);
+        Assert.assertEquals(session, Session.getActiveSession());
+      }
+	  }
 	
-	  boolean allowLoginUI = getIntent().getBooleanExtra("allowLoginUI", false);
+	  Intent intent = getIntent();
+    boolean allowLoginUI = intent.getBooleanExtra("allowLoginUI", false);
+
 	  // if no active open session, create one with the ui-enabled helper
     if (FacebookLib.isSessionValid() == false) {
 	    session = Session.openActiveSession(this, allowLoginUI, null);
+      
+      // if login didn't get at least a session, close this activity
+      if (session == null) {
+        //Extension.debug("CustomActivity:onCreate called finish()");
+        finish();
+        return;
+      }
     } 
+    if (loginActivity == null) {
+      loginActivity = this;
+    }
 
-	  // if login didn't get at least a session, close this activity
-	  if (session == null) {
-      Extension.debug("CustomActivity:onCreate called finish()");
-	    finish();
-	  }
+    // handle startActivity vis a vis showDialog
+    String method = intent.getStringExtra("method");
+    Bundle params = intent.getBundleExtra("params");
+    // don't restore these values on save/load - pause/resume
+    intent.putExtra("method", (String)null);
+    intent.putExtra("params", (Bundle)null);
+
+    if (("feed").equalsIgnoreCase(method)) {
+      Assert.assertTrue(session.isOpened());
+      Assert.assertNull(dialogActivity);
+      dialogActivity = this;
+      WebDialog feedDialog =
+        new WebDialog.FeedDialogBuilder(dialogActivity, Session.getActiveSession(), params)
+      .setOnCompleteListener(dialogCallback)
+      .build();
+      feedDialog.show();
+    }
+    else if (("apprequests").equalsIgnoreCase(method)) {
+      Assert.assertTrue(session.isOpened());
+      Assert.assertNull(dialogActivity);
+      dialogActivity = this;
+      WebDialog requestDialog =
+        new WebDialog.RequestsDialogBuilder(dialogActivity, Session.getActiveSession(), params)
+      .setOnCompleteListener(dialogCallback)
+      .build();
+      requestDialog.show();
+    }
   }
 
   @Override
   public void onResume() {
-    Extension.debug("CustomActivity:onResume");
+    Extension.debug("CustomActivity %d:onResume", activityId);
     super.onResume();
     uiHelper.onResume();
   }
 
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
-    Extension.debug("CustomActivity:onActivityResult %d, %d",requestCode, resultCode);
+    Extension.debug("CustomActivity %d:onActivityResult %d, %d", activityId, requestCode, resultCode);
     super.onActivityResult(requestCode, resultCode, data);
     uiHelper.onActivityResult(requestCode, resultCode, data);
   }
 
   @Override
   public void onPause() {
-    Extension.debug("CustomActivity:onPause");
+    Extension.debug("CustomActivity %d:onPause", activityId);
     super.onPause();
     uiHelper.onPause();
   }
 
   @Override
   public void onDestroy() {
-    Extension.debug("CustomActivity:onDestroy");
+    Extension.debug("CustomActivity %d:onDestroy", activityId);
+    // Extension.debug("onDestroy loginActivity %d dialogActivity %d", 
+    //      loginActivity != null ? loginActivity.activityId : -1, 
+    //      dialogActivity != null ? dialogActivity.activityId : -1);
+
     super.onDestroy();
     uiHelper.onDestroy();
-	uiHelper = null;
-	staticReference = null;
+    if (this == loginActivity) {
+      //Extension.debug("loginActivity nulled");
+      loginActivity = null;
+    }
+    if (this == dialogActivity) {
+      //Extension.debug("dialogActivity nulled");
+      dialogActivity = null;
+    }
   }
 
   @Override
   public void onSaveInstanceState(Bundle outState) {
-    Extension.debug("CustomActivity:onSaveInstanceState");
+    Extension.debug("CustomActivity %d:onSaveInstanceState", activityId);
     super.onSaveInstanceState(outState);
     uiHelper.onSaveInstanceState(outState);
   }
--- src/android/src/com/jesusla/facebook/FacebookLib.java
+++ src/android/src/com/jesusla/facebook/FacebookLib.java
@@ -19,17 +19,14 @@ import com.facebook.widget.*;
 import com.jesusla.ane.Context;
 import com.jesusla.ane.Extension;
 
-
-import java.lang.reflect.Field;
-
 public class FacebookLib extends Context {
-  static public FacebookLib staticReference;
-  public String applicationId;
-  public AccessToken oldAccessToken;
-  public Session.StatusCallback sessionStatusCallback;
+  static public String applicationId;
+  static public AccessToken oldAccessToken;
+  static public Session.StatusCallback sessionStatusCallback;
+  static public WebDialog.OnCompleteListener dialogCompleteCallback;
   
   public FacebookLib() {
-    FacebookLib.staticReference = this;
+    Extension.debug("FacebookLib::FacebookLib");
     registerFunction("applicationId", "getApplicationId");
     registerFunction("accessToken", "getAccessToken");
     registerFunction("expirationDate", "getExpirationDate");
@@ -45,114 +42,22 @@ public class FacebookLib extends Context {
     Extension.debug("FacebookLib::initContext");
     new RemapResourceIds(getActivity());
 	
-	sessionStatusCallback = new Session.StatusCallback() {
+	  sessionStatusCallback = new Session.StatusCallback() {
       @Override
-	  public void call(Session session, SessionState state, Exception exception) {
-	    // dispatch the login response for user initiated sessions but not my autosession
-		if (CustomActivity.staticReference.getIntent().getBooleanExtra("allowLoginUI", false)) {
-	      if (state == SessionState.CLOSED_LOGIN_FAILED) {
-	        dispatchStatusEventAsync("LOGIN_FAILED", "SESSION");
-	      }
-	      if ((state == SessionState.OPENED)||(state == SessionState.OPENED_TOKEN_UPDATED)) {
-	        dispatchStatusEventAsync("LOGIN", "SESSION");
-	      }
-		}
-	  }
+	    public void call(Session session, SessionState state, Exception exception) {
+	      // dispatch the login response for user initiated sessions but not my autosession
+		    if (CustomActivity.getLoginActivity().getIntent().getBooleanExtra("allowLoginUI", false)) {
+	        if (state == SessionState.CLOSED_LOGIN_FAILED) {
+	          dispatchStatusEventAsync("LOGIN_FAILED", "SESSION");
+	        }
+	        if ((state == SessionState.OPENED)||(state == SessionState.OPENED_TOKEN_UPDATED)) {
+	          dispatchStatusEventAsync("LOGIN", "SESSION");
+	        }
+		    }
+	    }
     };
 
-	
-	
-    // use our fbAppID instead of com.facebook.sdk.ApplicationId
-    applicationId = getProperty("FacebookAppID");
-	Extension.debug("FacebookAppID = "+ applicationId);
-
-    Settings.setShouldAutoPublishInstall(true); 
-	
-	// readOldAccessToken() {
-    SharedPreferences preferences = getActivity().getPreferences(Activity.MODE_PRIVATE);
-    String accessToken = preferences.getString("FBAccessTokenKey", null);
-    long accessExpires = preferences.getLong("FBExpirationDateKey", 0);
-    long lastAccessUpdate = preferences.getLong("FBLastAccessUpdate", 0);
-    if (accessToken != null) {
-      oldAccessToken = AccessToken.createFromExistingAccessToken(accessToken, new Date(accessExpires), new Date(lastAccessUpdate), null, null);
-	  // removeOldAccessToken() {
-      SharedPreferences.Editor editor = preferences.edit();
-      editor.remove("FBAccessTokenKey");
-      editor.remove("FBExpirationDateKey");
-      editor.remove("FBLastAccessUpdate");
-      editor.commit();
-    }
-	
-	// Previously, the sessionValid flag was used and extended to say: we know we've got credentials, just reuse them
-	// To emulate and improve that behavior, I attempt a silent login at launch, which takes the place of extending credentials from before
-	if ((isSessionValid() == false) || (CustomActivity.staticReference == null)) {
-	  startLoginActivity(false);
-	}
-  }
-
-  public String getApplicationId() {
-    return applicationId;
-  }
-
-  public String getAccessToken() {
-    if (isSessionValid()) {
-	  return Session.getActiveSession().getAccessToken();
-	}
-    return null;
-  }
-
-  public Date getExpirationDate() {
-	if (isSessionValid()) {
-	  Date expires = Session.getActiveSession().getExpirationDate();
-      return expires;
-	}
-	return null;
-  }
-  
-  public void login() {
-	Extension.debug("FacebookLib::login");
-	// Extension.debug("FacebookLib::isSessionValid = " + isSessionValid());
-	// Extension.debug("FacebookLib::customActivity = " + customActivity);
-	
-	// don't bother with the login unless necessary
-	if ((isSessionValid() == false) || (CustomActivity.staticReference == null)) {
-	  startLoginActivity(true);
-	}
-	else {
-	  dispatchStatusEventAsync("LOGIN", "SESSION");
-	}
-  }
-
-  private void startLoginActivity(boolean allowLoginUI) {
-	Intent intent = new Intent(getActivity(), CustomActivity.class);
-	intent.putExtra("allowLoginUI", allowLoginUI);
-    getActivity().startActivity(intent);
-  }
-
-  public void logout() {
-	if (isSessionValid()) {
-	  Session.getActiveSession().closeAndClearTokenInformation();
-	}
-	if (CustomActivity.staticReference != null) {
-	  CustomActivity.staticReference.finish();
-	}
-    dispatchStatusEventAsync("LOGOUT", "SESSION");
-  }
-
-  static public boolean isSessionValid() {
-    Session session = Session.getActiveSession();
-    if (session != null) {
-      return session.isOpened();
-    }
-    return false;
-  }
-
-  public void showDialog(String action, Bundle params) {
-    Session session = Session.getActiveSession();
-	  Assert.assertTrue(session.isOpened());
-	  Assert.assertNotNull(CustomActivity.staticReference);
-	  
-    WebDialog.OnCompleteListener onComplete = new WebDialog.OnCompleteListener() {
+    dialogCompleteCallback = new WebDialog.OnCompleteListener() {
       @Override
       public void onComplete(Bundle values, FacebookException error) {
         if (error == null) {
@@ -170,44 +75,121 @@ public class FacebookLib extends Context {
       }
     };
 
+    // use our fbAppID instead of com.facebook.sdk.applicationId
+    applicationId = getProperty("FacebookAppID");
+	  Extension.debug("FacebookAppID = "+ applicationId);
+
+    Settings.setShouldAutoPublishInstall(true); 
+	
+	  // readdOldAccessToken() {
+    SharedPreferences preferences = getActivity().getPreferences(Activity.MODE_PRIVATE);
+    String accessToken = preferences.getString("FBAccessTokenKey", null);
+    long accessExpires = preferences.getLong("FBExpirationDateKey", 0);
+    long lastAccessUpdate = preferences.getLong("FBLastAccessUpdate", 0);
+    if (accessToken != null) {
+      oldAccessToken = AccessToken.createFromExistingAccessToken(accessToken, new Date(accessExpires), new Date(lastAccessUpdate), null, null);
+	    // removeOldAccessToken() {
+      SharedPreferences.Editor editor = preferences.edit();
+      editor.remove("FBAccessTokenKey");
+      editor.remove("FBExpirationDateKey");
+      editor.remove("FBLastAccessUpdate");
+      editor.commit();
+    }
+	
+	  // Previously, the sessionValid flag was used and extended to say: we know we've got credentials, just reuse them
+	  // To emulate and improve that behavior, I attempt a silent login at launch, which takes the place of extending credentials from before
+	  if ((isSessionValid() == false) || (CustomActivity.getLoginActivity() == null)) {
+	    startLoginActivity(false);
+	  }
+  }
+
+  public String getApplicationId() {
+    return applicationId;
+  }
+
+  public String getAccessToken() {
+    if (isSessionValid()) {
+	    return Session.getActiveSession().getAccessToken();
+	  }
+    return null;
+  }
+
+  public Date getExpirationDate() {
+	  if (isSessionValid()) {
+	    Date expires = Session.getActiveSession().getExpirationDate();
+      return expires;
+	  }
+	  return null;
+  }
+  
+  public void login() {
+	  Extension.debug("FacebookLib::login");
+	  // Extension.debug("FacebookLib::isSessionValid = " + isSessionValid());
+	  // Extension.debug("FacebookLib::customActivity = " + customActivity);
+	
+	  // don't bother with the login unless necessary
+	  if ((isSessionValid() == false) || (CustomActivity.getLoginActivity() == null)) {
+	    startLoginActivity(true);
+	  }
+	  else {
+	    dispatchStatusEventAsync("LOGIN", "SESSION");
+	  }
+  }
+
+  private void startLoginActivity(boolean allowLoginUI) {
+	  Intent intent = new Intent(getActivity(), CustomActivity.class);
+	  intent.putExtra("allowLoginUI", allowLoginUI);
+    getActivity().startActivity(intent);
+  }
+
+  public void logout() {
+	  if (isSessionValid()) {
+	    Session.getActiveSession().closeAndClearTokenInformation();
+	  }
+	  if (CustomActivity.getLoginActivity() != null) {
+	    CustomActivity.getLoginActivity().finish();
+	  }
+    dispatchStatusEventAsync("LOGOUT", "SESSION");
+  }
+
+  static public boolean isSessionValid() {
+    Session session = Session.getActiveSession();
+    if (session != null) {
+      return session.isOpened();
+    }
+    return false;
+  }
+
+  public void showDialog(String action, Bundle params) {
     String method = params.getString("method");
-    if (method.equalsIgnoreCase("feed")) {
-      WebDialog feedDialog =
-        new WebDialog.FeedDialogBuilder(CustomActivity.staticReference, Session.getActiveSession(), params)
-      .setOnCompleteListener(onComplete)
-      .build();
-      feedDialog.show();
-    }
-    else if (method.equalsIgnoreCase("apprequests")) {
-      WebDialog requestDialog =
-        new WebDialog.RequestsDialogBuilder(CustomActivity.staticReference, Session.getActiveSession(), params)
-      .setOnCompleteListener(onComplete)
-      .build();
-      requestDialog.show();
-    }
+    Intent intent = new Intent(getActivity(), CustomActivity.class);
+    intent.putExtra("method", method);
+    intent.putExtra("params", params);
+
+    getActivity().startActivity(intent);
   }
 
   public String graph(String graphPath, Bundle params, String httpMethodString) {
     final String uuid = UUID.randomUUID().toString();
     Session session = Session.getActiveSession();
-	Assert.assertTrue(session.isOpened());
+	  Assert.assertTrue(session.isOpened());
 
     Request.Callback callback = new Request.Callback() {
       @Override
       public void onCompleted(Response response) {
-		if (response.getGraphObject() != null) {
-		  try {
+		    if (response.getGraphObject() != null) {
+		      try {
             JSONObject data = new JSONObject(response.getGraphObject().asMap());
             asyncFlashCall(null, null, "requestDidLoad", uuid, data);
           } 
-		  catch (NullPointerException e) {
+		      catch (NullPointerException e) {
             Extension.debug("Extension.fail Parsing '%s'", response.getGraphObject().asMap());
-		    asyncFlashCall(null, null, "requestDidFailWithError", uuid, e);
+		        asyncFlashCall(null, null, "requestDidFailWithError", uuid, e);
           }
-		}
-		else {
-		  asyncFlashCall(null, null, "requestDidFailWithError", uuid, response.getError());
-		}
+		    }
+		    else {
+		      asyncFlashCall(null, null, "requestDidFailWithError", uuid, response.getError());
+		    }
       }
     };
     HttpMethod httpMethod = HttpMethod.GET;
@@ -224,17 +206,18 @@ public class FacebookLib extends Context {
 
   private String encodeBundle(Bundle bundle) {
     String url = "fbconnect://success";
-	if (bundle != null ) {
-	  url += "?";
-	  for (String key : bundle.keySet()) {
-	    String val = bundle.getString(key);
-	    key = URLEncoder.encode(key);
-	    val = URLEncoder.encode(val);
-	    if (!url.endsWith("?"))
-  		  url += '&';
-	    url = url + key + '=' + val;
+	  if (bundle != null ) {
+	    url += "?";
+	    for (String key : bundle.keySet()) {
+	      String val = bundle.getString(key);
+	      key = URLEncoder.encode(key);
+	      val = URLEncoder.encode(val);
+	      if (!url.endsWith("?")) {
+          url += '&';  
+        }
+  		  url = url + key + '=' + val;
+	    }
 	  }
-	}
     return url;
   }
 }
